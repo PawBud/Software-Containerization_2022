7a0f9e42fa3e31a20d3187b9e2a58a1c43cd0d23
Punya Kapoor <50608826+punyakapoor@users.noreply.github.com>
2023-02-05 23:53:40 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -27,9 +27,21 @@ kubectl apply -f pv.yaml
 
 echo "helm deployement"
 
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 helm install -f  antiracismapp.yaml antiracismapp-frontend-helm /Users/punyakapoor/Documents/Software-Containerization_2022/og/Software-Containerization_2022/antiracismapp
 helm install -f  antiracismapp_backend.yaml antiracismapp-backend-helm /Users/punyakapoor/Documents/Software-Containerization_2022/og/Software-Containerization_2022/antiracismapp
 helm install -f  antiracismapp-db.yaml antiracismapp-db-helm /Users/punyakapoor/Documents/Software-Containerization_2022/og/Software-Containerization_2022/antiracismapp
+
+echo "Helm upgrade"
+
+helm upgrade -f ./antiracismapp-db/values.yaml -f override.yaml antiracismapp-db ./antiracismapp-db
+
+echo "helm uninstall"
+
+helm uninstall antiracism-db
 
 echo "view the deployements"
 
